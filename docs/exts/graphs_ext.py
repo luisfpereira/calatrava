e53e09e10cacf297e5812e09e9ea87011a770cb7
L. F. Pereira <luisfgpereira95@gmail.com>
2022-07-07 10:15:30 +0200

    Test readthedocs

--- docs/exts/graphs_ext.py
+++ docs/exts/graphs_ext.py
@@ -67,14 +67,13 @@ def cleanup(app, exception):
     srcdir = app.builder.srcdir
     outdir = app.builder.outdir
 
-    images_dir = os.path.join(outdir, '_images')
-    graphs_dir = _get_abspath_from_rel('_graphs', srcdir)
-    _copy_files(graphs_dir, images_dir)
-    logging.info(images_dir)
-    logging.info(images_dir)
+    # images_dir = os.path.join(outdir, '_images')
+    # graphs_dir = _get_abspath_from_rel('_graphs', srcdir)
+    # _copy_files(graphs_dir, images_dir)
+    # logging.info(images_dir)
+    # logging.info(images_dir)
 
     out_configs_dir = os.path.join(outdir, '_configs')
-
     os.makedirs(out_configs_dir, exist_ok=True)
     configs_dir = _get_abspath_from_rel(os.path.join(examples_dir, 'configs'), srcdir)
     _copy_files(configs_dir, out_configs_dir)
